c5b694cc284186aaad7e3739dde191f41545a7b3
Harshit Jain <harshitjainwork01@gmail.com>
2023-07-17 20:10:33 +0530

    added Pizza class for having pizza party

---
+++ harsg/src/Pizza.java
@@ -0,0 +1,21 @@
+public class Pizza {
+
+    String toppings;
+
+    int size;
+
+    String baseType;
+    /*
+    *
+    * Syntex of a method ( returnType FunctionName (Parameters pera)){
+    * // Operations
+    * }
+    * */
+
+    String getPipingHotPizza(Pizza pizza){
+        return "Your piping hot pizza is ready";
+    }
+
+
+
+}
