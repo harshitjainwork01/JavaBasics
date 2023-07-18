ffc0f0f9851255c93ab892d0b148703c3f4eb0d4
Harshit Jain <harshitjainwork01@gmail.com>
2023-07-18 08:07:52 +0530

    added this file

---
+++ harsg/src/BiskitFactory.java
@@ -0,0 +1,21 @@
+public class BiskitFactory {
+
+    //properties
+    int parleg;
+    int hindandsik;
+    int coconut;
+   static int zimzam=5;
+    String name;
+
+    //behaviour
+    void aata(BiskitFactory biskitFactory){
+        System.out.println(biskitFactory.name+"is making a biskit");
+    }
+    void cream(BiskitFactory biskitFactory){
+        System.out.println(biskitFactory.name+"is making ");
+    }
+    void maida(BiskitFactory biskitFactory){
+        System.out.println(biskitFactory.name+"is making");
+    }
+
+}
