ca9b42c812f4dde03a09f3e40d5ad89acccb97e0
Harshit Jain <harshitjainwork01@gmail.com>
2023-07-18 12:32:37 +0530

    is playing

---
+++ harsg/src/Garden.java
@@ -0,0 +1,22 @@
+public class Garden {
+
+    //properties
+    int cargame;
+    int riding;
+    int swiming;
+    int playground;
+    String name;
+
+
+    //behaveiour
+    void Swimingpool(Garden garden) {
+        System.out.println(garden.name + "playing the swimingpool");
+    }
+    void Playground(Garden garden) {
+        System.out.println(garden.name + "is playground");
+    }
+
+}
+
+
+
---
+++ harsg/src/Hotel.java
@@ -0,0 +1,17 @@
+public class Hotel {
+    //properties
+    int room;
+    int staff;
+    String name;
+
+    // behaviour
+    void playing(Hotel hotel){
+        System.out.println(hotel.name+"croom");
+
+        void
+
+
+
+
+    }
+}
